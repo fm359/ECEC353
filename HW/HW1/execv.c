903d43c68c5d8c552509e93390212b3a377154fb
Farhan Muhammad <farhan@wego.com>
2019-11-28 15:55:00 -0500

    Add other exec methods

---
+++ HW/HW1/execv.c
@@ -0,0 +1,40 @@
+# Author: Farhan Muhammad
+# Date:   01/17/2019
+
+# This program demonstrates how to fork a parent process and overwrite the child process
+# with an "ls -l -h" command using the execv() function
+
+#include <sys/types.h> /* pid_t                              */
+#include <sys/wait.h>  /* waitpid(), WEXITSTATUS()           */
+#include <unistd.h>    /* vfork()                            */
+#include <stdlib.h>    /* exit(), EXIT_SUCCESS, EXIT_FAILURE */
+#include <stdio.h>     /* printf(), fprintf(), stderr        */
+
+int main(int argc, char** argv)
+{
+        pid_t pid;
+		char *const paramArray[] = {"ls", "-l", "-h", NULL};
+
+		pid = vfork();
+
+        if (pid < 0) {
+                fprintf(stderr, "error -> failed to vfork()");
+                exit(EXIT_FAILURE);
+        }
+
+        if (pid > 0) {
+                int child_ret;
+
+                printf("parent -> pid: %i\n", getpid());
+                waitpid(pid, &child_ret, 0);
+                printf("parent -> child exited with code %i\n", WEXITSTATUS(child_ret));
+
+                exit(EXIT_SUCCESS);
+        } else {
+                printf("child -> pid: %i\n", getpid());
+                execv("/bin/ls", paramArray);
+
+                printf("child -> error: failed to execv 'ls'\n");
+				exit(EXIT_FAILURE);
+		}
+}
